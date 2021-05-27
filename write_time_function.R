0d9a4399e20a02d3b955c26e6b58f243970c5a02
Pranav Palli <pranavpalli42@gmail.com>
2021-05-27 15:56:52 -0500

    wrote final function for timebased comparisions in .csv files

--- write_time_function.R
+++ write_time_function.R
@@ -5,41 +5,48 @@ setwd("/Users/pranavpalli/Desktop/summer_21")
 data <- read.csv(file = "./data/data_soybean.csv") #data file
 
 #vectors
-genes = data$gene
+plant_times <- c("A", "B", "C") #vector of plants at R5, 5/6, 6 times
+cols <- c() #vector of all column names
+genes <- data$genes #genes column from data
 
 #write function based on time
 write_time <- function(plant_type){
   
-  #switch cases based on input
-  if(plant_type == 'wild_type'){
-    # wild_type plants
-    specific_data <- data[, c('A1_1', 'A1_2', 'A1_3', 'B1_1', 'B1_2', 'B1_3', 'C1_1', 'C1_2', 'C1_3')]
-    
-  } else if(plant_type == 'wr1'){
-    # wr1
-    specific_data <- data[, c('A2_1', 'A2_2', 'A2_3', 'B2_1', 'B2_2', 'B2_3', 'C2_1', 'C2_2', 'C2_3')]
-
-  } else if(plant_type == 'wr1_dgat'){
-    # wr1 + dgat
-    specific_data <- data[, c('A3_1', 'A3_2', 'A3_3', 'B3_1', 'B3_2', 'B3_3', 'C3_1', 'C3_2', 'C3_3')]
-
-  } else if(plant_type == 'wr1_kasII'){
-    # wr1 + kasII
-    specific_data <- data[, c('A4_1', 'A4_2', 'A4_3', 'B4_1', 'B4_2', 'B4_3', 'C4_1', 'C4_2', 'C4_3')]
-    
+  # plant_number based on plant_type for cols
+  plant_number = switch (plant_type,
+    "wild_type" = 1,
+    "wr1" = 2,
+    "wr1_dgat" = 3,
+    "wr1_kasii" = 4
+  )
+  
+  #function to decide cols
+  for(plant_time in plant_times){ #iterates through all values in plant_times
+    for(col_number in 1:3){ #from 1-3 in col_end
+      col_beg <- paste(plant_time, toString(plant_number), sep = '') #start of each col name i.e. "A1/2/3/4"
+      col_end <- paste('_', toString(col_number), sep = '') #end of each coll name i.e. "_1/2/3"
+      col <- paste(col_beg, col_end, sep = '') #combines both
+      
+      #adds each column to cols
+      cols <- append(cols, col)
+    }
   }
   
+  # final data that is written in each file
+  final_data <- data[, cols]
+  
   # rownames are genes
-  rownames(specific_data) <- genes
+  rownames(final_data) <- genes
   
   # write to files
   folder <- './data/time_function/'
   final_file <- paste(plant_type, '.csv', sep = '')
-  write.csv(specific_data, file = paste(folder, final_file, sep = ''))
+  write.csv(final_data, file = paste(folder, final_file, sep = ''))
+  
 }
 
-# executing function
-write_time('wild_type')
-write_time('wr1')
-write_time('wr1_dgat')
-write_time('wr1_kasII')
+# calling functions
+write_time("wild_type")
+write_time("wr1")
+write_time("wr1_dgat")
+write_time("wr1_kasii")
